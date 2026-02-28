2b62ea853d70e28d7ad233b44c227bfe948f261d
takenncs <116774376+Takennncs@users.noreply.github.com>
2026-02-28 03:44:22 +0200

    Update mail.php

--- config/mail.php
+++ config/mail.php
@@ -9,6 +9,7 @@ return [
     'encryption' => env('MAIL_ENCRYPTION', 'tls'),
     'from' => [
         'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Takenncs Framework'),
+        'name' => env('MAIL_FROM_NAME', 'Framework'),
     ],
+
 ];
